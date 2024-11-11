4be8d13e0981dce20158ddcfe4e31298e3c96ee3
Tom <tom.freeman@vodafone.com>
2024-11-11 10:00:01 +0000

    setup

---
+++ project_fetch/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "2.0.0"
+
+import logging
+import os
+import sys
+
+MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
+REPO_PATH = os.path.dirname(MODULE_PATH)
+
+# Configuring logs
+
+logger = logging.getLogger(__name__)
+logger.propagate = False
+handler = logging.StreamHandler(stream=sys.stdout)
+formatter = logging.Formatter("[%(levelname)5s] %(asctime)-15s  %(filename)-15.15s:%(lineno)03d - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
